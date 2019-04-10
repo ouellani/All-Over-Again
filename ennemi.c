e4ab2828e4a84cef30727a100584031c3aeeca66
ouellani <47660414+ouellani@users.noreply.github.com>
2019-04-10 17:57:02 +0100

    Add files via upload

---
+++ ennemi.c
@@ -0,0 +1,40 @@
+
+#include "ennemi.h"
+
+
+  void initEnnemi(ennemi *e)
+  
+  {
+
+      
+      e->posEnnemi.x= 0 ;
+      e->posEnnemi.y= 0 ;
+      e->tab[0]=IMG_Load("personnage0.png");
+    
+  e->tab[1]=IMG_Load("din1.png");
+  
+    e->tab[2]=IMG_Load("din2.png");
+    
+  e->tab[3]=IMG_Load("din3.png");
+      e->tab[4]=IMG_Load("din4.png");
+   
+   e->tab[5]=IMG_Load("din5.png");
+      e->tab[6]=IMG_Load("din6.png");
+   
+   e->tab[7]=IMG_Load("din7.png");
+     
+        
+    }
+
+  
+
+
+  void afficherEnnemi(ennemi enm, SDL_Surface *screen,int i)
+   
+ {
+      
+     SDL_BlitSurface (enm.tab[i],NULL,screen,&enm.posEnnemi);
+     SDL_Flip (screen);
+    
+    }
+
---
+++ ennemi.h
@@ -0,0 +1,33 @@
+#ifndef ENNEMI_H_INCLUDED
+
+#define ENNEMI_H_INCLUDED
+
+
+#include <stdlib.h>
+
+#include <stdio.h>
+
+#include <SDL/SDL.h>
+
+#include <SDL/SDL_image.h>
+
+#include <SDL/SDL_mixer.h>
+
+#include <SDL/SDL_ttf.h>
+
+ typedef struct ennemi
+ 
+ {
+     SDL_Surface *tab[8];
+     SDL_Rect posEnnemi;
+  }ennemi;
+
+
+ 
+void initEnnemi(ennemi *e);
+ 
+void afficherEnnemi(ennemi enm,SDL_Surface *screen,int i);
+ 
+
+#endif
+
